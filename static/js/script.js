b46b6777aa7d9d1beaf45ab1e32a5f82df2fa231
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-28 14:11:14 +0300

    queryselec... for html

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,11 @@
 // JavaScript
 document.addEventListener('DOMContentLoaded', () => {
+  const clothingList = document.getElementById('clothingList');
+  const cartItems = document.getElementById('cartItems');
+  const totalPriceElement = document.getElementById('totalPrice');
+  const cartIcon = document.getElementById('cartIcon');
+  const cart = [];
+
+  // Fetch clothing data from JSON and render it on the webpage
 
 });
-  
